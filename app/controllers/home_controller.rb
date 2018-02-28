c70811ace41d2021777781e69bbc6ddb40ea31ac
Your Name <dre.murguido@gmail.com>
2018-02-28 15:30:39 -0500

    Created and added ENV variables

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,15 +3,15 @@ class HomeController < ApplicationController
 
   def index
     GoogleMapsService.configure do |config|
-  config.key = 'AIzaSyBBPKKk-ycpViBGGkNbA43DXhtUXcLsrGY'
+  config.key = ENV["MAPS"]
   config.retry_timeout = 20
   config.queries_per_second = 10
   gmaps = GoogleMapsService::Client.new
   results = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
    @longitude = results[0][:geometry][:location][:lng]
    @latitude = results[0][:geometry][:location][:lat]
-  #
-  #
+  #gmaps.directions
+  #gmaps.geocode
   #  @routes = gmaps.directions(
   #   '1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA',
   #   '2400 Amphitheatre Parkway, Mountain View, CA 94043, USA',
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,23 +1,4 @@
 module HomeHelper
 
-  require 'httparty'
-  # directions = "https://maps.googleapis.com/maps/api/directions/json?origin=#{origin}&destination=#{destination}&key=AIzaSyBdxBB-BShpiyCbufyoyvhxIuz5eZk3Yh0"
-
-
-
-
-
-
-  def get_address(street_address)
-    geocode = "https://maps.googleapis.com/maps/api/geocode/json?address=#{street_address}&key=AIzaSyAGY3TSCSVmFPdYz_0HiLShcTLOfo3lKrY"
-    street_address.gsub(' ', '+')
-    response_geocode = HTTParty.get(geocode)
-    response.body
-  end
-
-  def current_location
-    url = "https://www.google.com/maps/@25.7988077,-80.2042434,15z"
-    response = HTTParty.get(url)
-    p response.body
-  end
+  
 end
