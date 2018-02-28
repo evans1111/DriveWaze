0dfccb9825b9d0baceb4145331a0353fbebff5a7
Your Name <dre.murguido@gmail.com>
2018-02-28 12:32:35 -0500

    landing page map

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,14 @@ class HomeController < ApplicationController
   require 'google_maps_service'
 
   def index
-  #   GoogleMapsService.configure do |config|
-  # config.key = 'AIzaSyBBPKKk-ycpViBGGkNbA43DXhtUXcLsrGY'
-  # config.retry_timeout = 20
-  # config.queries_per_second = 10
-  # gmaps = GoogleMapsService::Client.new
-  # results = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-  #  @longitude = results[0][:geometry][:location][:lng]
-  #  @latitude = results[0][:geometry][:location][:lat]
+    GoogleMapsService.configure do |config|
+  config.key = 'AIzaSyBBPKKk-ycpViBGGkNbA43DXhtUXcLsrGY'
+  config.retry_timeout = 20
+  config.queries_per_second = 10
+  gmaps = GoogleMapsService::Client.new
+  results = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+   @longitude = results[0][:geometry][:location][:lng]
+   @latitude = results[0][:geometry][:location][:lat]
   #
   #
   #  @routes = gmaps.directions(
@@ -17,7 +17,7 @@ class HomeController < ApplicationController
   #   '2400 Amphitheatre Parkway, Mountain View, CA 94043, USA',
   #   mode: 'walking',
   #   alternatives: false)
-  # end
+  end
 end
 
 
