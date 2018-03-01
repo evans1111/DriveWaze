244024e1d67600b1a8684fd5fc18f255276ef713
Alex Evans <aeboston1@gmail.com>
2018-03-01 15:05:48 -0500

    fixed feed - individual cards

--- app/controllers/spot_controller.rb
+++ app/controllers/spot_controller.rb
@@ -8,27 +8,27 @@ class SpotController < ApplicationController
     @spot = Spot.first(2)
 
     GoogleMapsService.configure do |config|
-  config.key = ENV["MAPS"]
-  config.retry_timeout = 20
-  config.queries_per_second = 10
-  gmaps = GoogleMapsService::Client.new
-  # query = [Spot.find(1).street, Spot.find(1).city, "FL"].join(', ')
-  #Grabs first 2 spots. Eventually grab all spots and translating
-  spots = Spot.first(2)
-  #map over all spots
-  gon.latlng = spots.map do |spot|
-    #creates query for gmaps
-    query = [spot.street, spot.city, "FL"].join(', ')
-    #translates to latitude/long
-    results = gmaps.geocode(query)
-    #parse data
+      config.key = ENV["MAPS"]
+      config.retry_timeout = 20
+      config.queries_per_second = 10
+      gmaps = GoogleMapsService::Client.new
 
-    results[0][:geometry][:location].values
+      # query = [Spot.find(1).street, Spot.find(1).city, "FL"].join(', ')
+      #Grabs first 2 spots. Eventually grab all spots and translating
+      spots = Spot.first(2)
+
+      #map over all spots
+      gon.latlng = spots.map do |spot|
+        #creates query for gmaps
+        query = [spot.street, spot.city, "FL"].join(', ')
+        #translates to latitude/long
+        results = gmaps.geocode(query)
+        #parse data
+        results[0][:geometry][:location].values
       end
-
-
     end
 
+    #Search function for feed - search by zip code
     @spots = if params[:zip]
       Spot.where('zip LIKE ?', "%#{params[:zip]}%")
     else
