b161e3e86fac98f4a9e4506920e2094f9b04b668
Asger <sirjazzper@gmail.com>
2018-03-03 20:23:35 -0500

    phone number field updated

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,11 +7,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :phone_number, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :email, :phone_number, :password, :password_confirmation, :current_password)
   end
 
   # GET /resource/sign_up
