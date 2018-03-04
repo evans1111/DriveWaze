cf28c7e9ece6e156fdf0a2ffb343a56e8eb43eef
Asger <sirjazzper@gmail.com>
2018-03-04 10:05:13 -0500

    signin/signout menu links fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # The path used after sign up.
+  def after_sign_up_path_for(resource)
+    dashboard_index_path
+  end
+
   def configure_permitted_parameters
 
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :remember_me)}
