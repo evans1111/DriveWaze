632e2b997663ddce2ef4b6c658421cfe83a8ece1
Alex Evans <aeboston1@gmail.com>
2018-03-06 15:46:56 -0500

    Availability time update, form styling

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -2,9 +2,16 @@ class Availability < ApplicationRecord
   belongs_to :spot
   has_many :bookings
 
-  def time_range
-    return "#{self.to.hour - 12} - #{self.from.hour - 12}"
+  def time_left
+    
   end
+
+  def normal_time
+    # Time.now.strftime("%I:%M %p")
+    return "#{self.to.strftime("%I:%M %p")} - #{self.from.strftime("%I:%M %p")}"
+  end
+
+
 
 
 
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :availability
+
+
+
+  def time_now
+    return time_now = Time.now.strftime("%I:%M %p")
+  end
+
 end
