532f4a61be3826005971f7cb220ccd2ee9c7cbbb
Alex Evans <aeboston1@gmail.com>
2018-03-07 14:32:26 -0500

    updated reservation countdown timer

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :availability
 
-  def time_left
+  def time_remaining
     current_time = Time.now
     finish_time = self.availability.from
     time_left = finish_time - current_time
@@ -11,7 +11,18 @@ class Booking < ApplicationRecord
     minutes = (time_left / 60) % 60
     hours = (time_left / (60 * 60))
 
-    return format("%02d:%02d:%02d", hours, minutes, seconds)
+    if time_left >= 0
+      return format('%02d:%02d:%02d', hours, minutes, seconds)
+    else
+      return false
+    end
+  end
+
+
+  def out_of_time
+    if time_left <= 0
+      return false
+    end
   end
 
   def time_now
