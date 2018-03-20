2e6fa8b57fe3247b58d9e02782ac6b89ac1e57af
Alex Evans <aeboston1@gmail.com>
2018-03-20 11:35:05 -0400

    Navbar user state logic

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -62,7 +62,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    return dashboard_index_path
+    redirect_to dashboard_index_path
   end
 
   # The path used after sign up for inactive accounts.
